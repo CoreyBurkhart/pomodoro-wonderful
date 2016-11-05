b147ac350f7497ce883b6761357584f8d012e12a
Corey Burkhart <hello@coreyburkhart.com>
2016-11-05 13:30:52 -0700

    added slider input lock & higher state

--- src/components/Body-dependencies/Slider.js
+++ src/components/Body-dependencies/Slider.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
 
 class Slider extends Component {
-  constructor(props) {
-    super(props);
-    // this.props = props;
-    this.state = {value: this.props.max / 2};
-    this.update = this.update.bind(this);
-  }
-
-  update(event) {
-    this.setState({value: event.target.value});
-  }
 
   render() {
     return (
       <div className="col-md-6">
-        <label htmlFor={this.props.title}>{this.props.title}</label>
-        <input type='range' min={this.props.min} max={this.props.max} onInput={this.update} />
-        <span>{this.state.value}</span>
+        <label htmlFor={this.props.title}> {this.props.title} </label>
+        <input type='range' min={this.props.min} id={this.props.title} max={this.props.max} onInput={this.props.inputHandler} disabled={this.props.disabled}/>
+        <span>{this.props.value}</span>
       </div>
     )
   }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import Slider from './Body-dependencies/Slider';
+// import Clock from './Body-dependencies/Clock';
+import Lock from './Body-dependencies/Lock'
 
 class Body extends Component {
+
+  //state should be here since both the Slider and Clock need to share the value of the imputs
+  constructor() {
+    super();
+    this.lockHandler = this.lockHandler.bind(this);
+    this.inputHandler = this.inputHandler.bind(this);
+    this.state = {'locked': false, 'Work': '', 'Break': ''};
+  }
+
+  lockHandler(e) {
+    this.setState({'locked': !this.state.locked});
+    console.log(this.state.locked);
+    if (this.state.locked === true) {
+      //send the times to the Clock component
+    }
+  }
+
+  inputHandler(event) {
+    if (event.target.id === 'Work') {
+      this.setState({'locked': this.state.locked, 'Work': event.target.value, 'Break': this.state.Break});
+    }
+    else{
+      this.setState({'locked': this.state.locked, 'Work': this.state.Work, 'Break':  event.target.value})
+
+    }
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="row sliders">
         <div className='col-md-8 col-md-offset-2'>
-          <Slider  min='0' max='120' title="Work"/>
-          <Slider  min='0' max='30' title="Break"/>
+          <Slider  value={this.state.Work} min='0' max='120' title="Work" disabled={this.state.locked} inputHandler={this.inputHandler}/>
+          <Slider  value={this.state.Break} min='0' max='30' title="Break" disabled={this.state.locked} inputHandler={this.inputHandler} />
         </div>
+        <Lock click={this.lockHandler}/>
+        {/* <Clock /> */}
       </div>
     );
   }
